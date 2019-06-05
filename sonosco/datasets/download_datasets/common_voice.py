98f0943da99670a803cf1a6aa6b429082c7d845b
ga38nif <florian.lay@tum.de>
2019-06-06 00:48:14 +0200

    start refactoring common_voice

--- sonosco/datasets/download_datasets/common_voice.py
+++ sonosco/datasets/download_datasets/common_voice.py
@@ -1,27 +1,32 @@
 import os
 import wget
+import click
+import logging
 import tarfile
-import argparse
+import shutil
 import csv
+import sonosco.common.audio_tools as audio_tools
+import sonosco.common.path_utils as path_utils
 from multiprocessing.pool import ThreadPool
 import subprocess
-from utils import create_manifest
+from sonosco.datasets.download_datasets.data_utils import create_manifest
+from sonosco.common.utils import setup_logging
 
-parser = argparse.ArgumentParser(description='Downloads and processes Mozilla Common Voice dataset.')
-parser.add_argument("--target-dir", default='CommonVoice_dataset/', type=str, help="Directory to store the dataset.")
-parser.add_argument("--tar-path", type=str, help="Path to the Common Voice *.tar file if downloaded (Optional).")
-parser.add_argument('--sample-rate', default=16000, type=int, help='Sample rate')
-parser.add_argument('--min-duration', default=1, type=int,
-                    help='Prunes training samples shorter than the min duration (given in seconds, default 1)')
-parser.add_argument('--max-duration', default=15, type=int,
-                    help='Prunes training samples longer than the max duration (given in seconds, default 15)')
-parser.add_argument('--files-to-process', default="cv-valid-dev.csv,cv-valid-test.csv,cv-valid-train.csv",
-                    type=str, help='list of *.csv file names to process')
-args = parser.parse_args()
+logger = logging.getLogger("sonosco")
+
 COMMON_VOICE_URL = "https://common-voice-data-download.s3.amazonaws.com/cv_corpus_v1.tar.gz"
 
+@click.command()
+@click.option("--target-dir", default="temp/data/libri_speech", type=str, help="Directory to store the dataset.")
+@click.option("--sample-rate", default=16000, type=int, help="Sample rate.")
+@click.option("--files-to-use", multiple=True,
+              default=["cv-valid-dev.csv","cv-valid-test.csv","cv-valid-train.csv"])
+@click.option("--min-duration", default=1, type=int,
+              help="Prunes training samples shorter than the min duration (given in seconds).")
+@click.option("--max-duration", default=15, type=int,
+              help="Prunes training samples longer than the max duration (given in seconds).")
 
-def convert_to_wav(csv_file, target_dir):
+def convert_to_wav(csv_file, target_dir, sample_rate):
     """ Read *.csv file description, convert mp3 to wav, process text.
         Save results to target_dir.
     Args:
@@ -30,8 +35,8 @@ def convert_to_wav(csv_file, target_dir):
     """
     wav_dir = os.path.join(target_dir, 'wav/')
     txt_dir = os.path.join(target_dir, 'txt/')
-    os.makedirs(wav_dir, exist_ok=True)
-    os.makedirs(txt_dir, exist_ok=True)
+    path_utils.try_create_directory(wav_dir)
+    path_utils.try_create_directory(txt_dir)
     path_to_data = os.path.dirname(csv_file)
 
     def process(x):
@@ -42,7 +47,7 @@ def convert_to_wav(csv_file, target_dir):
             f.write(text)
         cmd = "sox {} -r {} -b 16 -c 1 {}".format(
             os.path.join(path_to_data, file_path),
-            args.sample_rate,
+            sample_rate,
             os.path.join(wav_dir, file_name + '.wav'))
         subprocess.call([cmd], shell=True)
 
@@ -54,36 +59,40 @@ def convert_to_wav(csv_file, target_dir):
             pool.map(process, data)
 
 
-def main():
-    target_dir = args.target_dir
-    os.makedirs(target_dir, exist_ok=True)
+def main(target_dir, sample_rate, files_to_use, min_duration, max_duration):
+    setup_logging(logger)
 
-    target_unpacked_dir = os.path.join(target_dir, "CV_unpacked")
-    os.makedirs(target_unpacked_dir, exist_ok=True)
+    path_to_data = os.path.join(os.path.expanduser("~"), target_dir)
+    path_utils.try_create_directory(path_to_data)
 
-    if args.tar_path and os.path.exists(args.tar_path):
-        print('Find existing file {}'.format(args.tar_path))
-        target_file = args.tar_path
-    else:
-        print("Could not find downloaded Common Voice archive, Downloading corpus...")
-        filename = wget.download(COMMON_VOICE_URL, target_dir)
-        target_file = os.path.join(target_dir, os.path.basename(filename))
+    target_unpacked_dir = os.path.join(target_dir, "common_unpacked")
+    path_utils.try_create_directory(target_unpacked_dir)
+
+    extracted_dir = os.path.join(path_to_data, "CommonVoice")
+    if os.path.exists(extracted_dir):
+        shutil.rmtree(extracted_dir)
+
+    path_utils.try_download(target_unpacked_dir, COMMON_VOICE_URL)
+
+    logger.info("Download complete")
+    logger.info("Unpacking...")
 
     print("Unpacking corpus to {} ...".format(target_unpacked_dir))
-    tar = tarfile.open(target_file)
-    tar.extractall(target_unpacked_dir)
+    tar = tarfile.open(target_unpacked_dir)
+    tar.extractall(extracted_dir)
     tar.close()
 
     for csv_file in args.files_to_process.split(','):
-        convert_to_wav(os.path.join(target_unpacked_dir, 'cv_corpus_v1/', csv_file),
-                       os.path.join(target_dir, os.path.splitext(csv_file)[0]))
+        convert_to_wav(os.path.join(extracted_dir, 'cv_corpus_v1/', csv_file),
+                       os.path.join(target_dir, os.path.splitext(csv_file)[0]),
+                       sample_rate)
 
     print('Creating manifests...')
-    for csv_file in args.files_to_process.split(','):
+    for csv_file in files_to_use.split(','):
         create_manifest(os.path.join(target_dir, os.path.splitext(csv_file)[0]),
                         os.path.splitext(csv_file)[0] + '_manifest.csv',
-                        args.min_duration,
-                        args.max_duration)
+                        min_duration,
+                        max_duration)
 
 
 if __name__ == "__main__":
