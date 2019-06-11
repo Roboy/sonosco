028f1aeda36d60b9cf3921928dd34ed379b0b179
ga38nif <florian.lay@tum.de>
2019-06-11 17:49:27 +0200

    adapt common_voice to click logger [Needs test with good internet connectivity]

--- sonosco/datasets/download_datasets/common_voice.py
+++ sonosco/datasets/download_datasets/common_voice.py
@@ -11,13 +11,14 @@ from multiprocessing.pool import ThreadPool
 import subprocess
 from sonosco.datasets.download_datasets.data_utils import create_manifest
 from sonosco.common.utils import setup_logging
+from sonosco.common.constants import *
 
-logger = logging.getLogger("sonosco")
+logger = logging.getLogger(__name__)
 
 COMMON_VOICE_URL = "https://common-voice-data-download.s3.amazonaws.com/cv_corpus_v1.tar.gz"
 
 @click.command()
-@click.option("--target-dir", default="temp/data/libri_speech", type=str, help="Directory to store the dataset.")
+@click.option("--target-dir", default="temp/data/common_voice", type=str, help="Directory to store the dataset.")
 @click.option("--sample-rate", default=16000, type=int, help="Sample rate.")
 @click.option("--files-to-use", multiple=True,
               default=["cv-valid-dev.csv","cv-valid-test.csv","cv-valid-train.csv"])
@@ -25,6 +26,44 @@ COMMON_VOICE_URL = "https://common-voice-data-download.s3.amazonaws.com/cv_corpu
               help="Prunes training samples shorter than the min duration (given in seconds).")
 @click.option("--max-duration", default=15, type=int,
               help="Prunes training samples longer than the max duration (given in seconds).")
+def try_download_common_voice(target_dir, sample_rate, files_to_use, min_duration, max_duration):
+    path_to_data = os.path.join(os.path.expanduser("~"), target_dir)
+    path_utils.try_create_directory(path_to_data)
+
+    target_unpacked_dir = os.path.join(path_to_data, "common_unpacked")
+    path_utils.try_create_directory(target_unpacked_dir)
+
+    extracted_dir = os.path.join(path_to_data, "CommonVoice")
+    if os.path.exists(extracted_dir):
+        shutil.rmtree(extracted_dir)
+    logger.info("Start downloading...")
+    file_name = COMMON_VOICE_URL.split("/")[-1]
+    target_filename = os.path.join(target_unpacked_dir, file_name)
+    path_utils.try_download(target_filename, COMMON_VOICE_URL)
+
+    logger.info("Download complete")
+    logger.info("Unpacking...")
+    tar = tarfile.open(target_filename)
+    tar.extractall(extracted_dir)
+    tar.close()
+    os.remove(target_unpacked_dir)
+    assert os.path.exists(extracted_dir), f"Archive {file_name} was not properly uncompressed"
+    logger.info("Converting files to wav and extracting transcripts...")
+    for csv_file in files_to_use.split(','):
+        convert_to_wav(os.path.join(extracted_dir, 'cv_corpus_v1/', csv_file),
+                       os.path.join(target_dir, os.path.splitext(csv_file)[0]),
+                       sample_rate)
+    logger.info(f"Finished {COMMON_VOICE_URL}")
+    shutil.rmtree(extracted_dir)
+
+    logger.info('Creating manifests...')
+    for csv_file in files_to_use.split(','):
+        create_manifest(os.path.join(path_to_data, os.path.splitext(csv_file)[0]),
+                        os.path.splitext(csv_file)[0] + '_manifest.csv',
+                        min_duration,
+                        max_duration)
+
+
 
 def convert_to_wav(csv_file, target_dir, sample_rate):
     """ Read *.csv file description, convert mp3 to wav, process text.
@@ -51,48 +90,17 @@ def convert_to_wav(csv_file, target_dir, sample_rate):
             os.path.join(wav_dir, file_name + '.wav'))
         subprocess.call([cmd], shell=True)
 
-    print('Converting mp3 to wav for {}.'.format(csv_file))
+    logger.ino('Converting mp3 to wav for {}.'.format(csv_file))
     with open(csv_file) as csvfile:
         reader = csv.DictReader(csvfile)
         data = [(row['filename'], row['text']) for row in reader]
         with ThreadPool(10) as pool:
             pool.map(process, data)
 
-
-def main(target_dir, sample_rate, files_to_use, min_duration, max_duration):
+def main(**kwargs):
+    logger = logging.getLogger(SONOSCO)
     setup_logging(logger)
-
-    path_to_data = os.path.join(os.path.expanduser("~"), target_dir)
-    path_utils.try_create_directory(path_to_data)
-
-    target_unpacked_dir = os.path.join(target_dir, "common_unpacked")
-    path_utils.try_create_directory(target_unpacked_dir)
-
-    extracted_dir = os.path.join(path_to_data, "CommonVoice")
-    if os.path.exists(extracted_dir):
-        shutil.rmtree(extracted_dir)
-
-    path_utils.try_download(target_unpacked_dir, COMMON_VOICE_URL)
-
-    logger.info("Download complete")
-    logger.info("Unpacking...")
-
-    print("Unpacking corpus to {} ...".format(target_unpacked_dir))
-    tar = tarfile.open(target_unpacked_dir)
-    tar.extractall(extracted_dir)
-    tar.close()
-
-    for csv_file in args.files_to_process.split(','):
-        convert_to_wav(os.path.join(extracted_dir, 'cv_corpus_v1/', csv_file),
-                       os.path.join(target_dir, os.path.splitext(csv_file)[0]),
-                       sample_rate)
-
-    print('Creating manifests...')
-    for csv_file in files_to_use.split(','):
-        create_manifest(os.path.join(target_dir, os.path.splitext(csv_file)[0]),
-                        os.path.splitext(csv_file)[0] + '_manifest.csv',
-                        min_duration,
-                        max_duration)
+    try_download_common_voice(**kwargs)
 
 
 if __name__ == "__main__":
