d3796279c59883d4002ce14dd5cdf078775841a5
ga38nif <florian.lay@tum.de>
2019-06-12 09:18:00 +0200

    adapt an4 to new style and make it work.

--- sonosco/datasets/download_datasets/an4.py
+++ sonosco/datasets/download_datasets/an4.py
@@ -1,60 +1,84 @@
-import argparse
 import os
+import click
 import io
 import shutil
 import tarfile
-import wget
+import logging
+import sonosco.common.audio_tools as audio_tools
+import sonosco.common.path_utils as path_utils
 
-from utils import create_manifest
+from sonosco.datasets.download_datasets.data_utils import create_manifest
+from sonosco.common.utils import setup_logging
+from sonosco.common.constants import *
 
-parser = argparse.ArgumentParser(description='Processes and downloads an4.')
-parser.add_argument('--target-dir', default='an4_dataset/', help='Path to save dataset')
-parser.add_argument('--min-duration', default=1, type=int,
-                    help='Prunes training samples shorter than the min duration (given in seconds, default 1)')
-parser.add_argument('--max-duration', default=15, type=int,
-                    help='Prunes training samples longer than the max duration (given in seconds, default 15)')
-args = parser.parse_args()
+logger = logging.getLogger(__name__)
 
+AN4_URL = 'http://www.speech.cs.cmu.edu/databases/an4/an4_raw.bigendian.tar.gz'
 
-def _format_data(root_path, data_tag, name, wav_folder):
-    data_path = args.target_dir + data_tag + '/' + name + '/'
-    new_transcript_path = data_path + '/txt/'
-    new_wav_path = data_path + '/wav/'
+def try_download_an4(target_dir, sample_rate, min_duration, max_duration):
+    path_to_data = os.path.join(os.path.expanduser("~"), target_dir)
+    if not os.path.exists(path_to_data):
+        os.makedirs(path_to_data)
+    target_unpacked_dir = os.path.join(path_to_data, "an4_unpacked")
+    path_utils.try_create_directory(target_unpacked_dir)
 
-    os.makedirs(new_transcript_path)
-    os.makedirs(new_wav_path)
+    extracted_dir = os.path.join(path_to_data, "An4")
+    if os.path.exists(extracted_dir):
+        shutil.rmtree(extracted_dir)
+    logger.info("Start downloading...")
+    file_name = AN4_URL.split("/")[-1]
 
-    wav_path = root_path + 'wav/'
-    file_ids = root_path + 'etc/an4_%s.fileids' % data_tag
-    transcripts = root_path + 'etc/an4_%s.transcription' % data_tag
-    train_path = wav_path + wav_folder
+    target_filename = os.path.join(target_unpacked_dir, file_name)
+    path_utils.try_download(target_filename, AN4_URL)
+    logger.info("Download complete")
+    logger.info("Unpacking...")
+    tar = tarfile.open(target_filename)
+    tar.extractall(extracted_dir)
+    tar.close()
+    assert os.path.exists(extracted_dir), f"Archive {file_name} was not properly uncompressed"
+    logger.info("Converting files to wav and extracting transcripts...")
 
-    _convert_audio_to_wav(train_path)
-    _format_files(file_ids, new_transcript_path, new_wav_path, transcripts, wav_path)
+    create_wav_and_transcripts(path_to_data, 'train', sample_rate, extracted_dir, 'an4_clstk')
+    create_wav_and_transcripts(path_to_data, 'test', sample_rate, extracted_dir, 'an4test_clstk')
 
+    create_manifest(path_to_data, os.path.join(path_to_data,'an4_train_manifest.csv'), min_duration, max_duration)
+    create_manifest(path_to_data, os.path.join(path_to_data,'an4_val_manifest.csv'), min_duration, max_duration)
 
-def _convert_audio_to_wav(train_path):
+def create_wav_and_transcripts(path, data_tag, sample_rate, extracted_dir, wav_subfolder_name):
+    tag_path = os.path.join(path,data_tag)
+    transcript_path_new = os.path.join(tag_path, 'txt')
+    wav_path_new = os.path.join(tag_path, 'wav')
+
+    path_utils.try_create_directory(transcript_path_new)
+    path_utils.try_create_directory(wav_path_new)
+
+    wav_path_ext = os.path.join(extracted_dir, 'an4/wav')
+    file_ids = os.path.join(extracted_dir, f'an4/etc/an4_{data_tag}.fileids')
+    transcripts_ext = os.path.join(extracted_dir, f'an4/etc/an4_{data_tag}.transcription')
+    path = os.path.join(wav_path_ext, wav_subfolder_name)
+    convert_audio_to_wav(path, sample_rate)
+    format_files(file_ids, transcript_path_new, wav_path_new, transcripts_ext, wav_path_ext)
+
+def convert_audio_to_wav(train_path, sample_rate):
     with os.popen('find %s -type f -name "*.raw"' % train_path) as pipe:
         for line in pipe:
             raw_path = line.strip()
             new_path = line.replace('.raw', '.wav').strip()
-            cmd = 'sox -t raw -r %d -b 16 -e signed-integer -B -c 1 \"%s\" \"%s\"' % (
-                16000, raw_path, new_path)
-            os.system(cmd)
+            audio_tools.transcode_recordings_an4(raw_path=raw_path, wav_path= new_path, sample_rate=sample_rate)
 
 
-def _format_files(file_ids, new_transcript_path, new_wav_path, transcripts, wav_path):
+def format_files(file_ids, new_transcript_path, new_wav_path, transcripts, wav_path):
     with open(file_ids, 'r') as f:
         with open(transcripts, 'r') as t:
             paths = f.readlines()
             transcripts = t.readlines()
             for x in range(len(paths)):
-                path = wav_path + paths[x].strip() + '.wav'
+                path = os.path.join(wav_path, paths[x].strip()) + '.wav'
                 filename = path.split('/')[-1]
                 extracted_transcript = _process_transcript(transcripts, x)
                 current_path = os.path.abspath(path)
-                new_path = new_wav_path + filename
-                text_path = new_transcript_path + filename.replace('.wav', '.txt')
+                new_path = os.path.join(new_wav_path ,filename)
+                text_path = os.path.join(new_transcript_path,filename.replace('.wav', '.txt'))
                 with io.FileIO(text_path, "w") as file:
                     file.write(extracted_transcript.encode('utf-8'))
                 os.rename(current_path, new_path)
@@ -64,23 +88,20 @@ def _process_transcript(transcripts, x):
     extracted_transcript = transcripts[x].split('(')[0].strip("<s>").split('<')[0].strip().upper()
     return extracted_transcript
 
+@click.command()
+@click.option("--target-dir", default="temp/data/an4", type=str, help="Directory to store the dataset.")
+@click.option("--sample-rate", default=16000, type=int, help="Sample rate.")
+@click.option("--min-duration", default=1, type=int,
+              help="Prunes training samples shorter than the min duration (given in seconds).")
+@click.option("--max-duration", default=15, type=int,
+              help="Prunes training samples longer than the max duration (given in seconds).")
 
-def main():
-    root_path = 'an4/'
-    name = 'an4'
-    wget.download('http://www.speech.cs.cmu.edu/databases/an4/an4_raw.bigendian.tar.gz')
-    tar = tarfile.open('an4_raw.bigendian.tar.gz')
-    tar.extractall()
-    os.makedirs(args.target_dir)
-    _format_data(root_path, 'train', name, 'an4_clstk')
-    _format_data(root_path, 'test', name, 'an4test_clstk')
-    shutil.rmtree(root_path)
-    os.remove('an4_raw.bigendian.tar.gz')
-    train_path = args.target_dir + '/train/'
-    test_path = args.target_dir + '/test/'
-    print ('\n', 'Creating manifests...')
-    create_manifest(train_path, 'an4_train_manifest.csv', args.min_duration, args.max_duration)
-    create_manifest(test_path, 'an4_val_manifest.csv')
+def main(**kwargs):
+    """Processes and downloads an4 dataset."""
+    global logger
+    logger = logging.getLogger(SONOSCO)
+    setup_logging(logger)
+    try_download_an4(**kwargs)
 
 
 if __name__ == '__main__':
