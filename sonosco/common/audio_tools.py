31ae0266730d9d99f4ebb323fe24e4404e968513
ga38nif <florian.lay@tum.de>
2019-06-12 11:14:44 +0200

    add transcription function for ted3

--- sonosco/common/audio_tools.py
+++ sonosco/common/audio_tools.py
@@ -10,3 +10,6 @@ def transcode_recording(source, destination, sample_rate):
 
 def transcode_recordings_an4(raw_path, wav_path, sample_rate):
     subprocess.call([f'sox -t raw -r {sample_rate} -b 16 -e signed-integer -B -c 1 \"{raw_path}\" \"{wav_path}\"'], shell=True)
+
+def transcode_recordings_ted3(source, destination, start_time, end_time, sample_rate):
+    subprocess.call([f"sox {source}  -r {sample_rate} -b 16 -c 1 {destination} trim {start_time} ={end_time}"],shell=True)
