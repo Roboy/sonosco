678095d49ecdefe3dbb4100bd334418d5f3f5292
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2019-06-17 18:22:54 +0200

    add experiment

--- sonosco/common/utils.py
+++ sonosco/common/utils.py
@@ -1,6 +1,11 @@
 import logging
-import os
 import numpy as np
+import os
+import subprocess
+import os.path as path
+
+from shutil import copyfile
+from typing import Tuple
 
 
 def setup_logging(logger: logging.Logger, filename=None, verbosity=False):
@@ -24,3 +29,65 @@ def setup_logging(logger: logging.Logger, filename=None, verbosity=False):
 
 def random_float(low: float, high: float):
     return np.random.random() * (high - low) + low
+
+
+def copy_code(source_dir, dest_dir, exclude_dirs: Tuple[str] = tuple(), exclude_files: Tuple[str] = tuple()):
+    """
+    Copies code from source_dir to dest_dir. Excludes specified folders and files by substring-matching.
+    Parameters:
+        source_dir (string): location of the code to copy
+        dest_dir (string): location where the code should be copied to
+        exclude_dirs (list of strings): folders containing strings specified in this list will be ignored
+        exclude_files (list of strings): files containing strings specified in this list will be ignored
+    """
+    source_basename = path.basename(source_dir)
+    for root, dirs, files in os.walk(source_dir, topdown=True):
+
+        # skip ignored dirs
+        if any(ex_subdir in root for ex_subdir in exclude_dirs):
+            continue
+
+        # construct destination dir
+        cropped_root = root[2:] if (root[:2] == './') else root
+        subdir_basename = path.basename(cropped_root)
+
+        # do not treat the root as a subdir
+        if subdir_basename == source_basename:
+            subdir_basename = ""
+        dest_subdir = os.path.join(dest_dir, subdir_basename)
+
+        # create destination folder
+        if not os.path.exists(dest_subdir):
+            os.makedirs(dest_subdir)
+
+        # copy files
+        for filename in filter(lambda x: not any(substr in x for substr in exclude_files), files):
+            source_file_path = os.path.join(root, filename)
+            dest_file_path = os.path.join(dest_subdir, filename)
+            copyfile(source_file_path, dest_file_path)
+
+
+def retrieve_git_hash():
+    """
+    Retrieves and returns the current gith hash if execution location is a git repo.
+    """
+    try:
+        git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+        return git_hash
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+    return False
+
+
+def save_run_params_in_file(folder_path, run_config):
+    """
+    Receives a run_config class, retrieves all member variables and saves them
+    in a config file for logging purposes.
+    Parameters:
+        folder_path - output folder
+        filename - output filename
+        run_config - shallow class with parameter members
+    """
+    with open(path.join(folder_path, "run_params.conf"), 'w') as run_param_file:
+        for attr, value in sorted(run_config.__dict__.items()):
+            run_param_file.write(f"{attr}: {value}\n")
--- sonosco/training/experiment.py
+++ sonosco/training/experiment.py
@@ -1,11 +1,11 @@
 import os
 import os.path as path
 import sys
-from time import time
 import datetime
+import sonosco.common.path_utils as path_utils
 
-from .tee import Tee
-from .utils import *
+from time import time
+from sonosco.common.utils import copy_code
 
 
 class Experiment:
@@ -22,43 +22,48 @@ class Experiment:
         >>> print(experiment.plots) # path to experiment plots
     """
 
-    def __init__(self, experiment_name, experiments_path=None, exclude_dirs=[], exclude_files=[]):
+    def __init__(self,
+                 experiment_name,
+                 experiments_path=None,
+                 sub_directories=("plots", "logs", "code"),
+                 exclude_dirs=('__pycache__', '.git', 'experiments'),
+                 exclude_files=('.pyc',)):
+
         self.experiments_path = self._set_experiments_dir(experiments_path)
         self.name = self._set_experiment_name(experiment_name)
-        self.path = path.join(self.experiments_path, self.name) # path to current experiment
-        self._sub_directories = ['plots', 'logs', 'code'] # default sub-directories
+        self.path = path.join(self.experiments_path, self.name)     # path to current experiment
+        self.logs = path.join(self.experiments_path, "logs")
+        self.code = path.join(self.experiments_path, "code")
+        self._sub_directories = sub_directories
 
-        self._exclude_dirs = ['__pycache__', '.git', 'experiments']
+        self._exclude_dirs = exclude_dirs
         self._exclude_dirs.extend(exclude_dirs)
-        self._exclude_files = ['.pyc']
+        self._exclude_files = exclude_files
         self._exclude_files.extend(exclude_files)
 
-
         self._init_directories()
-        self._tee = Tee(path.join(self.logs, 'console_output.log'), 'w') # start to log console
         self._copy_sourcecode()
 
-    def _set_experiments_dir(self, experiments_path):
-        if experiments_path != None:
+    @staticmethod
+    def _set_experiments_dir(experiments_path):
+        if experiments_path is not None:
             return experiments_path
+
         local_path = os.path.dirname(sys.argv[0])
         local_path = local_path if local_path != '' else './'
         return path.join(local_path, "experiments")
 
-    def _set_experiment_name(self, experiment_name):
+    @staticmethod
+    def _set_experiment_name(experiment_name):
         date_time = datetime.datetime.fromtimestamp(time()).strftime('%Y-%m-%d_%H:%M:%S')
-        return date_time + "_" + experiment_name
+        return f"{date_time}_{experiment_name}"
 
     def _init_directories(self):
         """ Create all basic directories. """
-        self._create_directory(self.experiments_path)
-        self._create_directory(path.join(self.experiments_path, self.name))
+        path_utils.try_create_directory(self.experiments_path)
+        path_utils.try_create_directory(path.join(self.experiments_path, self.name))
         for sub_dir_name in self._sub_directories:
             self.add_directory(sub_dir_name)
-
-    def _create_directory(self, dir_path):
-        if not path.exists(dir_path):
-            os.makedirs(dir_path)
 
     def _add_member(self, key, value):
         """ Add a member variable named 'key' with value 'value' to the experiment instance. """
@@ -68,7 +73,9 @@ class Experiment:
         """ Copy code from execution directory in experiment code directory. """
         sources_path = os.path.dirname(sys.argv[0])
         sources_path = sources_path if sources_path != '' else './'
-        copy_code(sources_path, self.code, exclude_dirs=self._exclude_dirs, exclude_files=self._exclude_files )# exclude_dirs=[path.basename(self.experiments_path), '.vscode', '.git'])
+        copy_code(sources_path, self.code,
+                  exclude_dirs=self._exclude_dirs,
+                  exclude_files=self._exclude_files)
 
     def add_directory(self, dir_name):
         """
@@ -76,15 +83,16 @@ class Experiment:
         created and provided to the outside as a member variable.
         """
         # store in sub-dir list
-        if not dir_name in self._sub_directories:
+        if dir_name not in self._sub_directories:
             self._sub_directories.append(dir_name)
         # add as member
         dir_path = path.join(self.experiments_path, self.name, dir_name)
         self._add_member(dir_name, dir_path)
         # create directory
-        self._create_directory(dir_path)
+        path_utils.try_create_directory(dir_path)
 
-    def add_file(self, folder_path, filename, content):
+    @staticmethod
+    def add_file(folder_path, filename, content):
         """ Adds a file with provided content to folder. Convenience function. """
-        with open(path.join(folder_path, filename), 'w') as textfile:
-            textfile.write(content)
+        with open(path.join(folder_path, filename), 'w') as text_file:
+            text_file.write(content)
