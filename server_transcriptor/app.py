098159213e34cd7b4624ed7a880614ce2dad8d93
w.jurasz <w.jurasz@kigroup.de>
2019-07-09 01:36:43 +0200

    Server using multiple models

--- server_transcriptor/app.py
+++ server_transcriptor/app.py
@@ -11,6 +11,7 @@ from sonosco.models import DeepSpeech2
 from sonosco.decoders import GreedyDecoder
 from sonosco.datasets.processor import AudioDataProcessor
 from utils import get_config
+from model_loader import load_models
 
 app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
 CORS(app)
@@ -18,13 +19,16 @@ socketio = SocketIO(app)
 
 config = get_config('../server_transcriptor/config.yaml')
 
-model_path = "../pretrained/librispeech_pretrained.pth"
+# model_path = "../pretrained/librispeech_pretrained.pth"
 audio_path = "audio.wav"
 
 device = torch.device("cpu")
-model = DeepSpeech2.load_model(model_path)
-decoder = GreedyDecoder(model.labels, blank_index=model.labels.index('_'))
-processor = AudioDataProcessor(**model.audio_conf, normalize=True)
+# model = DeepSpeech2.load_model(model_path)
+loaded_models = load_models(config['models'])
+
+
+# decoder = GreedyDecoder(model.labels, blank_index=model.labels.index('_'))
+# processor = AudioDataProcessor(**model.audio_conf, normalize=True)
 
 
 @app.route('/', defaults={'path': ''})
@@ -35,16 +39,21 @@ def index(path):
 
 
 @socketio.on('record')
-def on_create(wav_bytes, models):
+def on_create(wav_bytes, model_ids):
     with open("audio.wav", "wb") as file:
         file.write(wav_bytes)
-    spect = processor.parse_audio(audio_path)
-    spect = spect.view(1, 1, spect.size(0), spect.size(1))
-    spect = spect.to(device)
-    input_sizes = torch.IntTensor([spect.size(3)]).int()
-    out, output_sizes = model(spect, input_sizes)
-    decoded_output, decoded_offsets = decoder.decode(out, output_sizes)
-    emit("transcription", decoded_output[0])
+        output = {}
+    for model_id in model_ids:
+        # TODO: move to load script
+        model_config = loaded_models[model_id]
+        spect = model_config['processor'].parse_audio(audio_path)
+        spect = spect.view(1, 1, spect.size(0), spect.size(1))
+        spect = spect.to(device)
+        input_sizes = torch.IntTensor([spect.size(3)]).int()
+        out, output_sizes = model_config['model'](spect, input_sizes)
+        decoded_output, decoded_offsets = model_config['decoder'].decode(out, output_sizes)
+        output[model_id] = decoded_output[0]
+    emit("transcription", output)
 
 
 @app.route('/get_models')
---
+++ server_transcriptor/model_loader.py
@@ -0,0 +1,18 @@
+from sonosco.models import DeepSpeech2
+from sonosco.decoders import GreedyDecoder
+from sonosco.datasets.processor import AudioDataProcessor
+
+# TODO: lazy loading
+def load_models(config):
+    models = {}
+    for model_config in config:
+        if model_config.get('script'):
+            pass  # TODO add possibility to add custom load script, not necessaryly in this way
+        else:
+            model_dict = {}
+            models[model_config['id']] = model_dict
+            model = DeepSpeech2.load_model(model_config['path'])
+            model_dict['model'] = model
+            model_dict['decoder'] = GreedyDecoder(model.labels, blank_index=model.labels.index('_'))
+            model_dict['processor'] = AudioDataProcessor(**model.audio_conf, normalize=True)
+    return models
