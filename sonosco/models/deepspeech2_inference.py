20fd159bd874df817e8eb9d9dcb23ab73356c8d0
ga38nif <florian.lay@tum.de>
2019-08-26 19:47:10 +0200

    rework inference part so that no file is saved (just in memory)

--- sonosco/models/deepspeech2_inference.py
+++ sonosco/models/deepspeech2_inference.py
@@ -2,7 +2,9 @@ from sonosco.inference.asr import SonoscoASR
 from sonosco.decoders import GreedyDecoder
 from sonosco.datasets.processor import AudioDataProcessor
 from sonosco.models.deepspeech2 import DeepSpeech2
+import tempfile
 import torch
+import librosa
 
 class DeepSpeech2Inference(SonoscoASR):
 
@@ -13,9 +15,10 @@ class DeepSpeech2Inference(SonoscoASR):
         self.cuda = cuda
 
     def infer(self, audio):
-        with open("audio.wav", "wb") as file:
-            file.write(audio)
-        spect = self.processor.parse_audio("audio.wav")
+        with tempfile.NamedTemporaryFile(delete=False) as temp_audio_file:
+            temp_audio_file.write(audio)
+        loaded, sr = librosa.load(temp_audio_file.name, sr=self.processor.sample_rate)
+        spect = self.processor.parse_audio(sound=loaded, sample_rate=sr)
         spect = spect.view(1, 1, spect.size(0), spect.size(1))
         spect = spect.to(torch.device("cuda" if self.cuda else "cpu"))
         input_sizes = torch.IntTensor([spect.size(3)]).int()
