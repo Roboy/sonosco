713230fc27238f0985cf4509fd0257f7b1d76742
w.jurasz <w.jurasz@kigroup.de>
2019-08-29 19:44:57 +0200

    Added checkpoint after epoch

--- sonosco/training/model_checkpoint.py
+++ sonosco/training/model_checkpoint.py
@@ -18,20 +18,24 @@ class ModelCheckpoint(AbstractCallback):
     """
 
     def __init__(self, output_path, model_name='model_checkpoint.pt'):
-        self.output_path = path.join(output_path, model_name)
+        self.output_path = output_path
+        self.model_name = model_name
         self.best_val_score = sys.float_info.max
         self.serializer = ModelSerializer()
 
     def __call__(self, epoch, step, performance_measures, context):
+        if step == (len(context.train_data_loader) - 1):
+            self._save_checkpoint(context.model, path.join(self.output_path, f"model_epoch_{epoch}.pt"))
+
         if 'val_loss' not in performance_measures:
             return
 
         if performance_measures['val_loss'] < self.best_val_score:
             self.best_val_score = performance_measures['val_loss']
-            self._save_checkpoint(context.model)
+            self._save_checkpoint(context.model, path.join(self.output_path, self.model_name))
 
-    def _save_checkpoint(self, model):
+    def _save_checkpoint(self, model, output_path):
         LOGGER.info("Saving model at checkpoint.")
         model.eval()
-        self.serializer.serialize_model(model=model, path=self.output_path)
+        self.serializer.serialize_model(model=model, path=output_path)
         model.train()
