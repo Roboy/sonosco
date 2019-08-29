e590343b74c9addf6ead63b72e136796d4a85bca
root <root@sonosco-data-instance.europe-west4-a.c.regal-cursor-221922.internal>
2019-08-29 17:47:55 +0000

    Added logging to train.py

--- executables/train.py
+++ executables/train.py
@@ -34,6 +34,7 @@ def main(config_path):
 
     loader = ModelDeserializer()
     if config.get('checkpoint_path'):
+        LOGGER.info("Starting from checkpoint")
         model = loader.deserialize_model(TDSSeq2Seq, config["checkpoint_path"])
     else:
         model = TDSSeq2Seq(config["encoder"], config["decoder"])
