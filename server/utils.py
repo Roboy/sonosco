edb90f6f52a264a952f80be817dc59ff3189ea4e
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2019-09-01 12:45:50 +0200

    start adding inference and add eval_las

--- server/utils.py
+++ server/utils.py
@@ -9,6 +9,7 @@ import os
 from datetime import datetime
 from sonosco.common.path_utils import try_create_directory
 
+
 def get_config(path='config.yaml'):
     return yaml.load(open(path), Loader=yaml.FullLoader)
 
@@ -27,6 +28,7 @@ def create_pseudo_db(db_path='~/.sonosco/audio_data/'):
     db_path = os.path.expanduser(db_path)
     pathlib.Path(db_path).mkdir(parents=True, exist_ok=True)
     return db_path
+
 
 def create_session_dir(sonosco_home):
     sesson_dir = os.path.join(sonosco_home, "sessions", datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
--- sonosco/inference/asr.py
+++ sonosco/inference/asr.py
@@ -2,16 +2,17 @@ from abc import ABC, abstractmethod
 
 import torch.nn as nn
 from sonosco.datasets import AudioDataProcessor
-
+from sonosco.model.deserializer import ModelDeserializer
 from sonosco.decoders import GreedyDecoder
 
 
 class SonoscoASR(ABC):
 
     # TODO: add processor
-    def __init__(self, model: nn.Module) -> None:
+    def __init__(self, model_path: str) -> None:
         super().__init__()
-        self.model = model
+        self.model_path = model_path
+        self.loader = ModelDeserializer()
 
     @abstractmethod
     def infer(self, sound_bytes): pass
