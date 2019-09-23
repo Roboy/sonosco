0436d008353df79d4a914aaf8f888ca3da024dcd
Yuriy Arabskyy <yuriy.arabskyy@gmail.com>
2019-09-23 14:06:19 +0200

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-docker run -v $1:/work/pretrained_host  -p 5000:5000 -p 8888:8888 -t sonosco_server
+
+if [ -z "$2" ]
+  then
+    echo "Using sonosco_server.."
+    image_name = "sonosco_server"
+  else
+    image_name = $2
+fi
+
+docker run -v $1:/work/pretrained_host  -p 5000:5000 -p 8888:8888 -t $image_name
