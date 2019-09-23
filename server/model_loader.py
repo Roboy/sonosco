7d9fd75b9c69409d660ab125311feceb24e912fd
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2019-09-23 14:58:41 +0200

    update model loader

--- server/model_loader.py
+++ server/model_loader.py
@@ -21,6 +21,10 @@ def load_models(config):
             pass
         else:
             try:
+                model_path = model_config["path"]
+                if not os.path.exists(model_path):
+                    logging.warning(f"Model {model_config['id']} does not exist in {model_path}. Skipping..")
+                    continue
                 models[model_config["id"]] = model_id_to_inference[model_config["id"]](model_config["path"])
             except Exception as e:
                 logging.error(f"Could not load {model_config['id']}, because of {e}")
--- server/run.sh
+++ server/run.sh
@@ -2,12 +2,14 @@
 
 if [ -z "$2" ]
   then
-    echo "Using sonosco_server.."
-    image="sonosco_server"
+    mkdir pretrained
+    pretrained_path="$(pwd)/pretrained"
+    echo "Downloading las model.."
+    wget -P $pretrained_path https://github.com/Roboy/sonosco/releases/download/v1.0-beta/las_model_5.pt
   else
-    image=$2
+    pretrained_path=$2
 fi
 
 echo "Running docker image $image"
 
-docker run -v $1:/work/pretrained_host -p 5000:5000 -p 8888:8888 -t $image
+docker run -v $pretrained_path:/work/pretrained_host -p 5000:5000 -p 8888:8888 -t $1
