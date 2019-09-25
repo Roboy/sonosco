66a140ccaf765c549c7584439cdcbc3f687030ac
w.jurasz <w.jurasz@kigroup.de>
2019-09-25 20:12:50 +0200

    Added docs for ASR

--- sonosco/inference/dummp_asr.py
+++ sonosco/inference/dummp_asr.py
@@ -4,10 +4,29 @@ from sonosco.inference.asr import SonoscoASR
 class DummyASR(SonoscoASR):
 
     def __init__(self) -> None:
+        """
+        Dummy implementation of ASR with fixed return values
+        """
         super().__init__("")
 
-    def infer(self, sound_bytes):
+    def infer(self, sound_bytes: any) -> str:
+        """
+
+        Args:
+            sound_bytes:
+
+        Returns: dummy transcript"
+
+        """
         return "dummy transcript"
 
     def infer_from_path(self, path: str) -> str:
+        """
+
+        Args:
+            path:
+
+        Returns: "dummy transcript"
+
+        """
         return "dummy transcript"
